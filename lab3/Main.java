94026a68293a10572a27a9bc1b29ff5388ebb200
anGit <55350092+anjm1020@users.noreply.github.com>
2022-03-22 16:47:53 +0900

    Feat : Upload lab3 template

---
+++ lab3/Main.java
@@ -0,0 +1,21 @@
+package bibimbap.study.jaemin;
+
+
+public class Main {
+
+    // Scanner 선언 및 생성
+
+    public static void main(String[] args) {
+
+        // 자신의 이름 출력
+        // 자신의 학번 출력
+
+        // A와 B를 받아 A@B 연산 결과를 출력하는 프로그램 작성
+        // A@B = (A+B)*(A-B)
+
+        // (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+        // (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+        // 세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성.
+
+    }
+}
